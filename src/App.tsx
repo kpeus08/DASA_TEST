1c6ef59b01fd3fafcab84ccbe4bc33e588c18b4d
kpeus08 <kall3.pet@gmail.com>
2022-05-14 13:42:08 +0200

    Created react-app with typescript and MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Button variant='contained'>
+          MUI-Button
+        </Button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
